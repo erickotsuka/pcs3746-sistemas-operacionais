f4d68262ab60f5753f3fa8e2adbbf7139e9f5522
Erick Otsuka <erick.r.otsuka@gmail.com>
2019-06-01 20:29:07 -0300

    Add initial semaphore test

--- 1/initramfs/semaphore_test.c
+++ 1/initramfs/semaphore_test.c
@@ -1,12 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+
 #include "semaphore.h"
 
 int main(void)
 {
-    for (;;)
+    int semaphore_id = init_semaphore(1);
+    pid_t pid_a = fork();
+    if (pid_a == -1)
     {
-        init_semaphore(7);
-        up(1);
-        down(1);
+        perror("fork");
+        return -1;
     }
+    else if (pid_a)
+    {
+       for (;;)
+       {
+           printf("A\n");
+           down(semaphore_id);
+           sleep(1);
+       }
+    }
+    else
+    {
+        for (;;)
+        {
+            printf("B\n");
+            up(semaphore_id);
+            sleep(4);
+        }
+    }
+
     return 0;
 }
